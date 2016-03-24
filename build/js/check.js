9d6b1eadac21a791644d3ac5db57fe90fd5b268f
birra05 <birra05@mail.ru>
2016-03-24 15:48:44 +0300

    Улучшила код

--- build/js/check.js
+++ build/js/check.js
@@ -1,5 +1,21 @@
 'use strict';
 
+function corruptedArea(a, b) {
+  var square = 0;
+  for (var i = 0; i < a.length && i < b.length; i++) {
+    square += a[i] * b[i];
+  }
+  return square;
+}
+
+function quantityDots(a) {
+  var sum = 0;
+  for (var x = 0; x < a.length; x++) {
+    sum += a[x];
+  }
+  return sum;
+}
+
 function getMessage(a, b) {
 
   if (a === true) {
@@ -9,16 +25,10 @@ function getMessage(a, b) {
   } else if (typeof a === 'number') {
     return ('Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов');
   } else if (a instanceof Array && b instanceof Array) {
-    var square = 0;
-    for (var i = 0; i < a.length && i < b.length; i++) {
-      square += a[i] * b[i];
-    }
+    var square = corruptedArea(a, b);
     return ('Общая площадь артефактов сжатия: ' + square + ' пикселей');
   } else if (a instanceof Array) {
-    var sum = 0;
-    for (var x = 0; x < a.length; x++) {
-      sum += a[x];
-    }
+    var sum = quantityDots(a);
     return ('Количество красных точек во всех строчках изображения: ' + sum);
   }
   return (a, b);
